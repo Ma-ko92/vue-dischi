0b926dbc89bf8ade1236cf4c630bdadc4ce09a17
Mario Koco <mario.koco92@gmail.com>
2021-04-28 17:51:35 +0200

    Esercizio base completato, sistemato css

--- js/script.js
+++ js/script.js
@@ -3,11 +3,23 @@ var app = new Vue({
     el: "#root",
 
     data: {
+        // Array of discs
+        discs: []
 
     },
 
     // Function
     methods: {
 
+    },
+    mounted() {
+        axios
+            // Boolean Api link
+            .get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((response) => {
+                const objArray = response.data;
+                this.discs = objArray.response
+                console.log(this.discs)
+            });
     }
 })
